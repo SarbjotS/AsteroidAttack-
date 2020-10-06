7e888b39c8706df31ca90fd0bb1d449ec1904122
SarbjotS <62689363+SarbjotS@users.noreply.github.com>
2020-10-06 15:16:52 +1300

    Asteroid works
    Work on collision next

--- src/JumpJump.java
+++ src/JumpJump.java
@@ -4,6 +4,7 @@ import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+
 public class JumpJump extends GameEngine implements KeyListener {
 	int WindowX = 700;
 	int WindowY = 700;
@@ -36,14 +37,96 @@ public class JumpJump extends GameEngine implements KeyListener {
 	boolean MU = false;
 	boolean MD = false;
 	boolean peaked = false;
+	boolean GameOver = false;
 	
 
+	//Asteroid
+	int AsteroidAmount;
+	double AsteroidX[], AsteroidY[];
+	double AsteroidSX[], AsteroidSY[];
+	double AsteroidVX[], AsteroidVY[];
+	double AsteroidAngle[];
+	Image AsteroidImage;
+	boolean going = true;
 	
+	//Asteroid difficulty
+	int AsteroidSpeed = 15;
+	int AsteroidDif = 1;
+	int counter = 0;
+
+	//Scores
+	int score = 0;
 	//----------------------
-	///////Load jump png 
-	///////FIX JUMP
-	
+	//Difficulty
+	//Collision
+	//Point System
+	//life system
+	//new game
 	//----------------------------
+	public void initAsteroid() {
+		AsteroidAmount = AsteroidDif; //INCREASE ASTEROIDS
+		AsteroidX = new double[AsteroidAmount];
+		AsteroidY = new double[AsteroidAmount];
+
+		AsteroidSX = new double[AsteroidAmount];
+		AsteroidSY = new double[AsteroidAmount];
+
+		AsteroidVX = new double[AsteroidAmount];
+		AsteroidVY = new double[AsteroidAmount];
+		AsteroidAngle = new double[AsteroidAmount];
+		
+		AsteroidImage = loadImage("Extras//meteor.png");
+		
+		for (int i = 0; i<AsteroidAmount; i++) {
+			newAsteroid(i);
+		}
+	}
+	public void newAsteroid(int i) {
+		AsteroidSX[i] = rand(700);
+		AsteroidSY[i] = 0;
+		
+		AsteroidX[i] = AsteroidSX[i];
+		AsteroidY[i] = AsteroidSY[i];
+		
+		double tx = rand(700);
+		double ty = 700;
+		AsteroidVX[i] = tx - AsteroidSX[i];
+		AsteroidVY[i] = ty - AsteroidSY[i];
+		AsteroidAngle[i] = atan2(AsteroidVX[i], -AsteroidVY[i]) - 90;
+		
+		double l = length(AsteroidVX[i], AsteroidVY[i]);
+		AsteroidVX[i] *= AsteroidSpeed/l; 
+		AsteroidVY[i] *= AsteroidSpeed/l;
+	}
+	
+	public void updateAsteroid(double dt) {
+		for (int i = 0; i <AsteroidAmount; i++) {
+			AsteroidX[i] += AsteroidVX[i]*dt;
+			AsteroidY[i] += AsteroidVY[i]*dt;
+			if (AsteroidY[i]>=700) {
+				System.out.print("Hey");
+			}
+		}
+	}
+	
+	public void drawAsteroid() {
+		for (int i =0; i<AsteroidAmount;i++) {
+			saveCurrentTransform();
+			translate(AsteroidX[i], AsteroidY[i]);
+			rotate(AsteroidAngle[i]);
+			drawImage(AsteroidImage,0,0,64,64);
+			restoreLastTransform();
+		}
+	}
+	
+	public void AsteroidCollision() {
+		for (int i = 0; i <AsteroidAmount; i++) {
+			if (AsteroidX[i] >= DinoX-64 && AsteroidX[i] <= DinoX+64 ||AsteroidY[i] >= DinoY-64 && AsteroidY[i] <= DinoY+64 ) {
+				System.out.print("YO");
+			}
+		}
+
+	}
 	
 	public void init() {
 		setWindowSize(WindowX,WindowY);
@@ -59,75 +142,96 @@ public class JumpJump extends GameEngine implements KeyListener {
 		IdleDino[8] = loadImage("IdleSprite\\Idle(9).png");
 		IdleDino[9] = loadImage("IdleSprite\\Idle(10).png");
 		
-		RunRightDino = new Image[5];
+		RunRightDino = new Image[8];
 		RunRightDino[0] = loadImage("RunRight\\RunRight(1).png");
 		RunRightDino[1] = loadImage("RunRight\\RunRight(2).png");
 		RunRightDino[2] = loadImage("RunRight\\RunRight(3).png");
 		RunRightDino[3] = loadImage("RunRight\\RunRight(4).png");
 		RunRightDino[4] = loadImage("RunRight\\RunRight(5).png");
+		RunRightDino[5] = loadImage("RunRight\\RunRight(6).png");
+		RunRightDino[6] = loadImage("RunRight\\RunRight(7).png");
+		RunRightDino[7] = loadImage("RunRight\\RunRight(8).png");
 
-		RunLeftDino = new Image[5];
+		RunLeftDino = new Image[8];
 		RunLeftDino[0] = loadImage("RunLeft\\RunLeft(1).png");
 		RunLeftDino[1] = loadImage("RunLeft\\RunLeft(2).png");
 		RunLeftDino[2] = loadImage("RunLeft\\RunLeft(3).png");
 		RunLeftDino[3] = loadImage("RunLeft\\RunLeft(4).png");
 		RunLeftDino[4] = loadImage("RunLeft\\RunLeft(5).png");
+		RunLeftDino[5] = loadImage("RunLeft\\RunLeft(6).png");
+		RunLeftDino[6] = loadImage("RunLeft\\RunLeft(7).png");
+		RunLeftDino[7] = loadImage("RunLeft\\RunLeft(8).png");
 
 		DinoJump = new Image[12];
 		DinoJump[0] = loadImage("DinoJump\\Jump (1).png");
 		DinoJump[1] = loadImage("DinoJump\\Jump (2).png");
 		DinoJump[2] = loadImage("DinoJump\\Jump (3).png");
 		DinoJump[3] = loadImage("DinoJump\\Jump (4).png");
-		DinoJump[4] = loadImage("DinoJump\\Jump (5).png");		RunLeftDino[0] = loadImage("RunLeft\\RunLeft(1).png");
+		DinoJump[4] = loadImage("DinoJump\\Jump (5).png");		
 		DinoJump[5] = loadImage("DinoJump\\Jump (6).png");
 		DinoJump[6] = loadImage("DinoJump\\Jump (7).png");
 		DinoJump[7] = loadImage("DinoJump\\Jump (8).png");
-		DinoJump[8] = loadImage("DinoJump\\Jump (9).png");		RunLeftDino[0] = loadImage("RunLeft\\RunLeft(1).png");
+		DinoJump[8] = loadImage("DinoJump\\Jump (9).png");		
 		DinoJump[9] = loadImage("DinoJump\\Jump (10).png");
 		DinoJump[10] = loadImage("DinoJump\\Jump (11).png");
 		DinoJump[11] = loadImage("DinoJump\\Jump (12).png");
 		
-		MainMenuGround = new Image[6];
+		MainMenuGround = new Image[3];
 		MainMenuGround[0] = loadImage("BasicGround\\Ground01.png");
 		MainMenuGround[1] = loadImage("BasicGround\\Ground02.png");
 		MainMenuGround[2] = loadImage("BasicGround\\Ground03.png");
-		MainMenuGround[3] = loadImage("BasicGround\\Ground03.png");
-		MainMenuGround[4] = loadImage("BasicGround\\Ground03.png");		
-		MainMenuGround[5] = loadImage("BasicGround\\Ground03.png");
 		drawImage(IdleDino[MenuCurrentFrame],DinoX,DinoY,200,200);
 		paintMainMenu();
 
 		if (PLAYING) {
 			StartGame();
 		}
-		
+
+
+		AsteroidCollision();
 		MoveDino();
-		//MAKE MAIN GAME HERE
 
 	}
 	
+
 	@Override
 	public void update(double dt) {
 		// TODO Auto-generated method stub
 		MenuCurrentFrame = (MenuCurrentFrame + 1) % 9;
-		currentFrame = (currentFrame +1) % 5;
+		currentFrame = (currentFrame +2) % 8;
 		JumpFrame = (JumpFrame+1)%12;
+		updateAsteroid(dt);
+		if (GameOver) {
+			return;
+		}
 
 		
 	}
 	public void StartGame(){
+		if (going) {
+			initAsteroid();
+			going = false;
+		}
+		//BackgroundImage = loadImage("Extras/BG.png");
 		drawImage(BackgroundImage,0,0,WindowX,WindowY);
 		drawImage(MainMenuGround[0],25,625,100,100);
 		drawImage(MainMenuGround[1],125,625,100,100);
-		drawImage(MainMenuGround[2],225,625,100,100);
-		drawImage(MainMenuGround[3],325,625,100,100);
-		drawImage(MainMenuGround[4],425,625,100,100);
-		drawImage(MainMenuGround[5],525,625,100,100);
-		
-		//MAKE A RANDOMIZER for where to place tiles
-		//drawImage(MainMenuGround[0],25,325,100,100);
-		//drawImage(MainMenuGround[1],125,325,100,100);
-		//drawImage(MainMenuGround[2],225,325,100,100);
+		drawImage(MainMenuGround[1],225,625,100,100);
+		drawImage(MainMenuGround[1],325,625,100,100);
+		drawImage(MainMenuGround[1],425,625,100,100);
+		drawImage(MainMenuGround[2],525,625,100,100);
+		drawAsteroid();
+
+		for(int i = 0; i<AsteroidAmount;i++) {
+			if (AsteroidY[i] >= 700) {
+				counter++;
+				if(counter == AsteroidDif) {
+					newAsteroid(i);
+					going = true;
+					counter = 0;
+				}
+			}
+		}
 	}
 	@Override
 	public void paintComponent() {
@@ -148,24 +252,22 @@ public class JumpJump extends GameEngine implements KeyListener {
 			drawImage(IdleDino[MenuCurrentFrame],DinoX,DinoY,200,200);
 		}
 		if (MR) {
-			DinoX+=5;
+			DinoX+=10;
 			drawImage(RunRightDino[currentFrame],DinoX,DinoY,200,200);
 		}
 		if (ML) {
-			DinoX-=5;
+			DinoX-=10;
 			drawImage(RunLeftDino[currentFrame],DinoX-64,DinoY,200,200); //WILL HAVE TO MINUS 64
 												//Flipped in editor so have to compensate for empty space
 		}
 		if (MU) {
 			DinoY-=10;
 			drawImage(DinoJump[JumpFrame], DinoX, DinoY,200,200);
+
 			}
 		if (MD) {
 			DinoY+=5;
-			//drawImage(IdleDino[JumpFrame], DinoX, DinoY,200,200);
-			if (MU && !MR || !ML) {
-				drawImage(IdleDino[JumpFrame], DinoX, DinoY,200,200);
-			}
+			IDLE = true;
 			if (DinoY == 450) {
 				MD = false;
 				MR = false;
@@ -181,7 +283,7 @@ public class JumpJump extends GameEngine implements KeyListener {
 		drawImage(BackgroundImage,0,0,WindowX,WindowY);
 		drawImage(MainMenuHead,25,50,400,50);
 
-
+		
 		drawImage(MainMenuGround[0],25,625,100,100);
 		drawImage(MainMenuGround[1],125,625,100,100);
 		drawImage(MainMenuGround[1],225,625,100,100);
@@ -243,12 +345,6 @@ public class JumpJump extends GameEngine implements KeyListener {
 			MU = true;
 			MR = false;
 			ML = false;
-			//if (e.getKeyCode() == KeyEvent.VK_A) {
-			//	DinoX-=10;
-			//}
-			//if (e.getKeyCode() == KeyEvent.VK_D) {
-			//	DinoX+=10;
-			//}
 		}	
 	}
 	
